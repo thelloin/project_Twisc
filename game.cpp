b1374fb911e3038bc4958a980cde8bbdbe6018c0
eried975 <eried975@localhost.ida.liu.se>
2014-11-18 14:12:33 +0100

    Skapat project och testfil

---
+++ game.cpp
@@ -0,0 +1,16 @@
+/*
+ * game.cpp
+ *
+ *  Created on: 18 nov 2014
+ *      Author: eried975
+ */
+#include <iostream>
+
+using namespace std;
+int main(int argc ,char* argv [])
+{
+	cout << "hello world" << endl;
+	return 0;
+}
+
+
